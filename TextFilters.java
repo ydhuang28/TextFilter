69a83bee0c313bf5b26b37ec561b748ab6e8bc5b
Yuxin David Huang <dhuang@colgate.edu>
2013-03-08 13:49:51 -0500

    Final Change
    Everything works as intended. Final version.

--- TextFilters.java
+++ TextFilters.java
@@ -1,173 +1,211 @@
 /*
  * TextFilters.java
+ *
+ * Group Members: Michael Lese, Yuxin David Huang
  * 
- * Author: Michael Lese
- * 		   Yuxin David Huang
- * 		   Colgate University
+ * Project Breakup:
+ *     Michael Lese -> Author of cat and diff
+ *     Yuxin David Huang -> Author of grep, tr, and uniq
+ * 
+ * Final version: March 8, 2013
  */
 
 import java.io.*;
 import java.util.*;
 
-public class TextFilters
-{
+/**
+ * A class that simulates some simple filters
+ * that exist in sh shells in Unix environments.
+ *
+ * @author Michael Lese, Yuxin David Huang, Vijay Ramachandran, Colgate University
+ */
+public class TextFilters {
 
-    private static void cat(String[] args)
-	{
+
+	/**
+	 * A method that simulates cat.
+	 *
+	 * If no argument is specified, will display stdin on stdout.
+	 *
+	 * If arguments are specified, then will display the contents of
+	 * the files given as arguments, one after the other, on stdout.
+	 
+	 * If there is an error with any given file, then will print an error
+	 * message to standard error (System.err, hereafter abbreviated
+	 * "stderr") indicating that the file could not be opened,
+	 * but will not terminate, and will continue on to the next file
+	 * in the argument list.
+	 *
+	 * @param args argument list
+	 */
+    private static void cat(String[] args) {
+	
 		InputStream in = null;
 		boolean file = false;
 		String filename = "stdin";
-		Scanner scan;
+		Scanner scan = null;
 
-		if (args.length==0) {
+		if (args.length == 0) {
 			scan = new Scanner(System.in);
 			System.out.print(scan.nextLine());
-		}
+		} else {
+			for (int i = 0; i < args.length; i++) {
+				filename = args[i];
+				try {
+					in = new FileInputStream(filename);
+					file = true;
+				} catch (FileNotFoundException e) {
+					System.err.println("cat: file " + filename + " was not found. Exiting.");
+					return;
+				} catch (SecurityException e) {
+					System.err.println("cat: file " + filename + " could not be opened. Access was denied.");
+					return;
+				}
+				scan = new Scanner(in);
 
-		for(int i=0; i<args.length; i++) {
-			filename=args[i];
-			try {
-				in = new FileInputStream(filename);
-				file=true;
-			} catch (FileNotFoundException e) {
-				System.err.println("cat error: file " + filename + " was not found. Exiting.");
-
-				return;
-			} catch (SecurityException e) {
-				System.err.println("cat error: file " + filename + " could not be opened. Access was denied.");
-				return;
+				while (scan.hasNextLine()) System.out.println(scan.nextLine());
 			}
-			scan = new Scanner(in);
-
-			while(scan.hasNext()) 
-				System.out.println(scan.nextLine());
 		}
-
+		
 		if (file) {
 			try {
 				in.close();
 			} catch (IOException e) {
-				System.err.println("sort: error closing file " + filename);
+				System.err.println("cat: error closing file " + filename);
 			}
 		}
-
-	}
+	} // end of cat(String[])
     
 
-    private static void diff(String[] args)
-    {
-    	String file1 = ""; 
-		String file2 = "";
+	/**
+	 * A method that simulates diff.
+	 *
+	 * diff takes 2 arguments. If less than 2 arguments
+	 * were specified, diff will quit, and print an error
+	 * message.
+	 *
+	 * When the arguments are given, diff will compare the 2
+	 * files given. It will print the lines that are in the first
+	 * file in the argument list but not in the second preceded
+	 * by a "-"; it will print the lines vice versa preceded by
+	 * a "+";
+	 *
+	 * @param args argument list
+	 */
+    private static void diff(String[] args) {
+	
+		String file1 = null;
+		String file2 = null;
 		InputStream in1 =  null;
 		InputStream in2 = null;
 		boolean f1 = false;
 		boolean f2 = false;
 
-
-		if(args.length ==0) {
-			System.err.println("diff error: missing arguement after `diff'");
+		if (args.length <= 0) {
+			System.err.println("diff: missing argument");
 			return;
-		}
-
-		if (args.length > 1) {
-			file1 += args[0];
-			file2 += args[1];
-			f1 = true;
-			f2 = true;
-		}
-
-		if(args.length == 1) {
-			System.err.println("diff error: missing input. Please type second filename.");
-			Scanner sc = new Scanner(System.in);
-			file1 += args[0]; 
-			file2 += sc.next();
-			f1 = true;
-			f2 = true;
-		}
-
-
-
-		try {
-			in1 = new FileInputStream(file1); 
-			f1 = true;
-		} catch (FileNotFoundException e) {
-			System.err.println("diff error: file " + file1 + " could not be found. Exiting.");
-			return;
-		} catch (SecurityException e) {
-			System.err.println("diff error: file " + file1 + " could not be opened. Access was denied.");
-			return;
-		}
-
-		try {
-			in2 = new FileInputStream(file2);
-		} catch (FileNotFoundException e) {
-			System.err.println("diff error: file " + file2 + " could not be found. Exiting.");
-			return;
-		} catch (SecurityException e) {
-			System.err.println("diff error: file " + file2 + " could not be opened. Access was denied.");
-			return;
+		} else {
+		
+			if (args.length == 1) {
+				System.err.println("diff: need filename of second file");
+				return;
+			} else if (args.length > 2) {
+				System.err.println("diff: too many arguments");
+				return;
+			} else {
+				file1 = args[0];
+				file2 = args[1];
+			
+				try {
+					in1 = new FileInputStream(file1); 
+					f1 = true;
+				} catch (FileNotFoundException e) {
+					System.err.println("diff error: file " + file1 + " could not be found. Exiting.");
+					return;
+				} catch (SecurityException e) {
+					System.err.println("diff error: file " + file1 + " could not be opened. Access was denied.");
+					return;
+				}
+			
+				try {
+					in2 = new FileInputStream(file2);
+				} catch (FileNotFoundException e) {
+					System.err.println("diff: file " + file2 + " could not be found. Exiting.");
+					return;
+				} catch (SecurityException e) {
+					System.err.println("diff: file " + file2 + " could not be opened. Access was denied.");
+					return;
+				}
+			}
+			
+			
 		}
 
 		Scanner scan1 = new Scanner(in1);
 		Scanner scan2 = new Scanner(in2);
 		boolean keep1 = false;
 		boolean keep2 = false;
-		String one = ""; 
-		String two = "";
+		String currLine1 = ""; 
+		String currLine2 = "";
 
-		while(scan1.hasNext() && scan2.hasNext()) {
-			//TODO: Fix weird algorithm glitches.
+		while (scan1.hasNext() && scan2.hasNext()) {
 
-			if (!keep1)
-				one = scan1.nextLine();
+			if (!keep1) currLine1 = scan1.nextLine();
+			if (!keep2) currLine2 = scan2.nextLine();
 
-			if (!keep2) 
-				two = scan2.nextLine();
-
-			if(one.compareTo(two) < 0)  {
-				System.out.println("- " + one);
+			if (currLine1.compareTo(currLine2) < 0)  {
+				System.out.println("- " + currLine1);
 				keep1 = false; 
 				keep2 = true;
-			}
-
-			if(one.compareTo(two) > 0) {
-				System.out.println("+ " + two);
+			} else if (currLine1.compareTo(currLine2) > 0) {
+				System.out.println("+ " + currLine2);
 				keep2 = false; 
 				keep1 = true;
-			}
-
-			if(one.compareTo(two) == 0) {
-				keep1= false;
+			} else {
+				keep1 = false;
 				keep2 = false;
 			}
+			
 		}
 
-		while(scan1.hasNext())
-			System.out.println("- " + scan1.nextLine());
+		if (!scan1.hasNextLine()) {
+			if (!currLine1.equals(currLine2)) {
+				System.out.println("+ " + currLine2);
+			}
+			while (scan2.hasNextLine()) System.out.println("+ " + scan2.nextLine());
+		} else if (!scan2.hasNextLine()) {
+			if (!currLine1.equals(currLine2)) {
+				System.out.println("+ " + currLine1);
+			}
+			while (scan1.hasNextLine()) System.out.println("- " + scan1.nextLine());
+		}
 
-		while(scan2.hasNext())
-			System.out.println("+ " + scan2.nextLine());
 
 
-
-		if (f1){
+		if (f1 || f2) {
 			try {
 				in1.close();
 			} catch (IOException e) {
 				System.err.println("diff: error closing file " + file1);
 			}
 		}
-
-		if (f2){
-			try {
-				in1.close();
-			} catch (IOException e) {
-				System.err.println("diff: error closing file " + file2);
-			}
-		}
-    }
+    } // end of diff(String[])
     
     
+	/**
+	 * A method that simulates grep.
+	 *
+	 * grep takes more than 1 arguments. If no arguments
+	 * are given then grep will print an error message 
+	 * and quit.
+	 *
+	 * grep will print the lines in the file (specified by
+	 * the second argument) that contains the string
+	 * specified in the first argument. If no second argument
+	 * is given, then it will take input from stdin.
+	 *
+	 * @param args argument list
+	 */
     private static void grep(String[] args) {
     
         InputStream in = null;      // input stream
@@ -183,7 +221,10 @@ public class TextFilters
             // check if the first argument is "-l" or the string to search for
             if ("-l".equals(args[0])) {
                 countLine = true;
-                if (args.length < 2) throw new IllegalArgumentException("grep: Not enough arguments"); 
+                if (args.length < 2) { 
+					System.err.println("grep: Not enough arguments"); 
+					return;	
+				}
             }
             
             if (countLine) {
@@ -211,8 +252,11 @@ public class TextFilters
                     }
                 }
                 
-                if (args.length > 3) throw new IllegalArgumentException("grep: Too many arguments");
-                
+                if (args.length > 3) {
+					System.err.println("grep: Too many arguments");
+					return;
+                }
+				
             } else {
                 target = args[0];
                 
@@ -235,7 +279,10 @@ public class TextFilters
                     }
                 }
                 
-                if (args.length > 2) throw new IllegalArgumentException("grep: Too many arguments");
+                if (args.length > 2) {
+					System.err.println("grep: Too many arguments");
+					return;
+				}
             }
             
             Scanner sc;
@@ -267,52 +314,87 @@ public class TextFilters
                 } catch (IOException e) {
                     System.err.print("grep: error closing file ");
                     System.err.println(filename);
+					return;
                 }
             }
             
-        } else throw new IllegalArgumentException("grep: Not enough arguments");
+        } else {
+			System.err.println("grep: Not enough arguments");
+			return;
+		}
     
     } // end of grep(String[])
     
     
+	/**
+	 * A method that simulates tr.
+	 *
+	 * tr does not need an argument. If no arguments
+	 * are given, then input will be taken from stdin.
+	 *
+	 * tr prints every word on every line on individual
+	 * lines.
+	 *
+	 * @param args argument list
+	 */
     private static void tr(String[] args) {
         
         InputStream in = null;      // input stream
         boolean file = false;       // used for error-msg printing
         String filename = null;     // default - no filename
+		Scanner sc = null;
         
-        if (args.length > 0) {
-            filename = args[0];
-            try {
-                in = new FileInputStream(filename);
-                file = true;
-            } catch (FileNotFoundException fife) {
-                System.err.print("tr: error, file ");
-                System.err.print(filename);
-                System.err.println(" not found or could not be opened");
-                return;
-            } catch (SecurityException se) {
-                System.err.print("tr: error, access to file ");
-                System.err.print(filename);
-                System.err.println(" was denied");
-                return;
-            }
-        } else {
-            in = System.in;
+        if (args.length <= 0) {
+			in = System.in;
+			sc = new Scanner(in);
+			while (sc.hasNextLine()) {
+				String currLine = sc.nextLine().trim();
+				String[] currWords = currLine.split(" ");
+				for (String word : currWords) {
+					System.out.println(word);
+				}
+			}
+		} else {
+			for (int i = 0; i < args.length; i++) {
+				filename = args[i];
+				try {
+					in = new FileInputStream(filename);
+					file = true;
+				} catch (FileNotFoundException e) {
+					System.err.println("tr: file " + filename + " was not found. Exiting.");
+					return;
+				} catch (SecurityException e) {
+					System.err.println("tr: file " + filename + " could not be opened. Access was denied.");
+					return;
+				}
+				scan = new Scanner(in);
+
+				while (sc.hasNextLine()) {
+					String currLine = sc.nextLine().trim();
+					String[] currWords = currLine.split(" ");
+					for (String word : currWords) {
+						System.out.println(word);
+					}
+				}	
+			}
         }
         
-        Scanner sc = new Scanner(in);
-        while (sc.hasNextLine()) {
-            String currLine = sc.nextLine().trim();
-            String[] currWords = currLine.split(" ");
-            for (String word : currWords) {
-                System.out.println(word);
-            }
-        }
+        
     
     } // end of tr(String[])
     
     
+	/**
+	 * A method that simulates uniq.
+	 *
+	 * tr does not need an argument. If no arguments
+	 * are given, then input will be taken from stdin.
+	 *
+	 * uniq will print the input with repeated lines
+	 * only once.
+	 *
+	 * @param args argument list
+	 */
     private static void uniq(String[] args) {
     
 		InputStream in = null;			// input stream
@@ -320,7 +402,10 @@ public class TextFilters
 		String filename = null;			// default - no file name
 		
 		if (args.length > 0) {
-			if (args.length > 1) throw new IllegalArgumentException("uniq: Too many arguments");
+			if (args.length > 1) {
+				System.err.println("uniq: Too many arguments");
+				return;
+			}
 		
 			filename = args[0];
 			try {
@@ -362,7 +447,6 @@ public class TextFilters
 		}
 		
     } // end of uniq(String[])
-    
     
     
     /**
@@ -542,5 +626,7 @@ public class TextFilters
             }
         }
         System.setIn(stdin);
+		
     } // end of main(String[])
+	
 } // end of TextFilters
